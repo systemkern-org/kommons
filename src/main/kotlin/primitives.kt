e98b60d181dc39d4dff6972e40c4b0cfeecd5a84
rainer <rainer@systemkern.com>
2019-01-08 15:26:55 +0100

    #21 Inline all primitive extension properties

--- src/main/kotlin/primitives.kt
+++ src/main/kotlin/primitives.kt
@@ -2,37 +2,37 @@ package com.systemkern.kommons
 
 import java.math.BigDecimal
 
-val Int.bd: BigDecimal get() = this.toBigDecimal().setScale(0)
+inline val Int.bd: BigDecimal get() = this.toBigDecimal().setScale(0)
 
-val Int.d: Double get() = this.toDouble()
+inline val Int.d: Double get() = this.toDouble()
 
-val Int.f: Float get() = this.toFloat()
+inline val Int.f: Float get() = this.toFloat()
 
-val Int.l: Long get() = this.toLong()
+inline val Int.l: Long get() = this.toLong()
 
-val Double.f: Float get() = this.toFloat()
+inline val Double.f: Float get() = this.toFloat()
 
-val Double.bd: BigDecimal get() = this.toBigDecimal()
+inline val Double.bd: BigDecimal get() = this.toBigDecimal()
 
-val Double.i: Int get() = this.toInt()
+inline val Double.i: Int get() = this.toInt()
 
-val Double.l: Long get() = this.toLong()
+inline val Double.l: Long get() = this.toLong()
 
-val Float.bd: BigDecimal get() = this.toBigDecimal()
+inline val Float.bd: BigDecimal get() = this.toBigDecimal()
 
-val Float.d: Double get() = this.toDouble()
+inline val Float.d: Double get() = this.toDouble()
 
-val Float.i: Int get() = this.toInt()
+inline val Float.i: Int get() = this.toInt()
 
-val Float.l: Long get() = this.toLong()
+inline val Float.l: Long get() = this.toLong()
 
-val Long.bd: BigDecimal get() = this.toBigDecimal().setScale(0)
+inline val Long.bd: BigDecimal get() = this.toBigDecimal().setScale(0)
 
-val Long.f: Float get() = this.toFloat()
+inline val Long.f: Float get() = this.toFloat()
 
-val Long.d: Double get() = this.toDouble()
+inline val Long.d: Double get() = this.toDouble()
 
-val Long.i: Int get() = this.toInt()
+inline val Long.i: Int get() = this.toInt()
 
 /** The Char array representing by this string */
-val String.chars: Array<Char> get() = this.toCharArray().toTypedArray()
+inline val String.chars: Array<Char> get() = this.toCharArray().toTypedArray()
