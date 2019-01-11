ac527c33a92662a403569c4ed6c76f2bb293a6e0
AndresAusecha <daausecham@unal.edu.co>
2019-01-11 17:53:59 -0500

    Correct decrypt line, correct deployment folder, add deployment sh

--- deployment/before-deploy.sh
+++ deployment/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_SOME_key -iv $encrypted_SOME_iv -in cd/signingkey.asc.enc -out cd/signingkey.asc -d
+    openssl aes-256-cbc -K $encrypted_c97290677436_key -iv $encrypted_c97290677436_iv -in keys.asc.enc -out keys.asc -d
     gpg --fast-import cd/signingkey.asc
 fi
