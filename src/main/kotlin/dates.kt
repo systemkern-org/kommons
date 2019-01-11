8adde5ca1c044bb515a18930f02528cbd33646d9
Alper Tekinalp <alper.tekinalp@gmail.com>
2019-01-11 09:36:30 +0300

    #24 Add string-date extensions

--- src/main/kotlin/dates.kt
+++ src/main/kotlin/dates.kt
@@ -1,8 +1,40 @@
 package com.systemkern.kommons
 
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeParseException
 
 /** @return the ISO 8601 yyyy-mm-dd string representation of this date */
-val LocalDate.isoStr: String
-    get() = DateTimeFormatter.ISO_LOCAL_DATE.format(this)
+fun LocalDate.format(format: String = "") : String {
+    val dateFormatter = if (format.isEmpty()) DateTimeFormatter.ISO_LOCAL_DATE else DateTimeFormatter.ofPattern(format)
+    return this.format(dateFormatter)
+}
+
+/** @return the ISO 8601 yyyy-mm-dd string representation of this date */
+fun LocalDateTime.format(format: String = ""): String {
+    val dateFormatter = if (format.isEmpty()) DateTimeFormatter.ISO_LOCAL_DATE_TIME else DateTimeFormatter.ofPattern(format)
+    return this.format(dateFormatter)
+}
+
+fun String.toLocalDate(format: String = ""): LocalDate? =
+    try {
+        val dateFormatter = if (format.isEmpty()) DateTimeFormatter.ISO_LOCAL_DATE else DateTimeFormatter.ofPattern(format)
+        LocalDate.parse(this, dateFormatter)
+    } catch (e: Exception) {
+        null
+    }
+
+fun String.toLocalDateTime(format: String = ""): LocalDateTime? {
+    return try {
+        val dateTimeFormatter = if (format.isEmpty()) DateTimeFormatter.ISO_LOCAL_DATE_TIME else DateTimeFormatter.ofPattern(format)
+        // first try to format as datetime
+        LocalDateTime.parse(this, dateTimeFormatter)
+    } catch (e: Exception) {
+        // if not try to format as date and convert to datetime on (0,0,0)
+        val date = this.toLocalDate(format) ?: return null
+        LocalDateTime.of(date, LocalTime.of(0,0,0))
+    }
+}
+
