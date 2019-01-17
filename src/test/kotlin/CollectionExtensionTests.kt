894846c73e21b9de4a8009dbaf54e3eb6cf6a7c4
rainer <rainer@systemkern.com>
2019-01-17 16:05:20 +0100

    #26 Fix jvm target configration

--- src/test/kotlin/CollectionExtensionTests.kt
+++ src/test/kotlin/CollectionExtensionTests.kt
@@ -1,7 +1,6 @@
 package com.systemkern.kommons
 
-import org.assertj.core.api.Assertions
-import org.assertj.core.api.Assertions.*
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 private class CollectionExtensionTests {
