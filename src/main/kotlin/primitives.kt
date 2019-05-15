063ff9abc4142390f735455103b4adbc919f3bd5
Rainer Kern <11457729+rainerkern@users.noreply.github.com>
2019-05-15 15:27:47 +0200

    Simplify Number extensions (#18)

--- src/main/kotlin/primitives.kt
+++ src/main/kotlin/primitives.kt
@@ -10,37 +10,22 @@ inline fun <T> Boolean.ifTrue(block: () -> T?): T? =
 inline fun <T> Boolean.ifFalse(block: () -> T?): T? =
     if (!this) block() else null
 
-inline val Int.bd: BigDecimal get() = this.toBigDecimal().setScale(0)
 
-inline val Int.d: Double get() = this.toDouble()
+inline val Number.d: Double get() = this.toDouble()
 
-inline val Int.f: Float get() = this.toFloat()
+inline val Number.f: Float get() = this.toFloat()
 
-inline val Int.l: Long get() = this.toLong()
+inline val Number.l: Long get() = this.toLong()
 
-inline val Double.f: Float get() = this.toFloat()
+inline val Number.i: Int get() = this.toInt()
 
 inline val Double.bd: BigDecimal get() = this.toBigDecimal()
 
-inline val Double.i: Int get() = this.toInt()
-
-inline val Double.l: Long get() = this.toLong()
-
 inline val Float.bd: BigDecimal get() = this.toBigDecimal()
 
-inline val Float.d: Double get() = this.toDouble()
-
-inline val Float.i: Int get() = this.toInt()
-
-inline val Float.l: Long get() = this.toLong()
+inline val Int.bd: BigDecimal get() = this.toBigDecimal().setScale(0)
 
 inline val Long.bd: BigDecimal get() = this.toBigDecimal().setScale(0)
-
-inline val Long.f: Float get() = this.toFloat()
-
-inline val Long.d: Double get() = this.toDouble()
-
-inline val Long.i: Int get() = this.toInt()
 
 /** The Char array representing by this string */
 inline val String.ch: Array<Char> get() = this.toCharArray().toTypedArray()
