979cb059d0581b0ed764f1afe8a49e5cbfea3410
AndresAusecha <daausecham@unal.edu.co>
2019-06-04 12:30:08 -0500

    add a new test

--- src/test/kotlin/SelfSortingMutableListTest.kt
+++ src/test/kotlin/SelfSortingMutableListTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test
 
 internal class SelfSortingMutableListTest {
 
-    @Test fun addSeveralSortedElements(){
+    @Test fun addAndSortSeveralElements(){
         val ssmList = SelfSortingMutableList<Int>()
         ssmList.add(8)
         ssmList.add(2)
@@ -18,6 +18,21 @@ internal class SelfSortingMutableListTest {
         assertThat(ssmList[2]).isEqualTo(3)
         assertThat(ssmList[3]).isEqualTo(4)
         assertThat(ssmList[4]).isEqualTo(8)
+    }
+
+    @Test fun addNegativeElements(){
+        val ssmList = SelfSortingMutableList<Int>()
+        ssmList.add(-8)
+        ssmList.add(-2)
+        ssmList.add(1)
+        ssmList.add(4)
+        ssmList.add(3)
+
+        assertThat(ssmList[0]).isEqualTo(-8)
+        assertThat(ssmList[1]).isEqualTo(-2)
+        assertThat(ssmList[2]).isEqualTo(1)
+        assertThat(ssmList[3]).isEqualTo(3)
+        assertThat(ssmList[4]).isEqualTo(4)
     }
 
 }
