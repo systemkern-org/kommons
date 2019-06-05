bd3276e08defd611d235b0e03911b63d74d3b3f2
Andres Ausecha Mosquera <daausecham@gmail.com>
2019-06-05 11:13:23 -0500

    #OSS-37 Add the Self Sorting Mutable List
    * add SelfSortingMutableList and a test
    * add a new test
    * add test with repeated

---
+++ src/test/kotlin/SelfSortingMutableListTest.kt
@@ -0,0 +1,53 @@
+package com.systemkern.kommons
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SelfSortingMutableListTest {
+
+    @Test fun addAndSortSeveralElements(){
+        val ssmList = SelfSortingMutableList<Int>()
+        ssmList.add(8)
+        ssmList.add(2)
+        ssmList.add(1)
+        ssmList.add(4)
+        ssmList.add(3)
+
+        assertThat(ssmList[0]).isEqualTo(1)
+        assertThat(ssmList[1]).isEqualTo(2)
+        assertThat(ssmList[2]).isEqualTo(3)
+        assertThat(ssmList[3]).isEqualTo(4)
+        assertThat(ssmList[4]).isEqualTo(8)
+    }
+
+    @Test fun addNegativeElements(){
+        val ssmList = SelfSortingMutableList<Int>()
+        ssmList.add(-8)
+        ssmList.add(-2)
+        ssmList.add(1)
+        ssmList.add(4)
+        ssmList.add(3)
+
+        assertThat(ssmList[0]).isEqualTo(-8)
+        assertThat(ssmList[1]).isEqualTo(-2)
+        assertThat(ssmList[2]).isEqualTo(1)
+        assertThat(ssmList[3]).isEqualTo(3)
+        assertThat(ssmList[4]).isEqualTo(4)
+    }
+
+    @Test fun addEqualElementsRepeated(){
+        val ssmList = SelfSortingMutableList<Int>()
+        ssmList.add(8)
+        ssmList.add(2)
+        ssmList.add(1)
+        ssmList.add(4)
+        ssmList.add(1)
+
+        assertThat(ssmList[0]).isEqualTo(1)
+        assertThat(ssmList[1]).isEqualTo(1)
+        assertThat(ssmList[2]).isEqualTo(2)
+        assertThat(ssmList[3]).isEqualTo(4)
+        assertThat(ssmList[4]).isEqualTo(8)
+    }
+
+}
