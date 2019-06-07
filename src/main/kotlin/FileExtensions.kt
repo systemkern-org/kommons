4be6f8ba30fc41ff66d54c1a9c5ed35e206aa8d6
AndresAusecha <daausecham@unal.edu.co>
2019-06-07 11:34:42 -0500

    add new functions and tests

--- src/main/kotlin/FileExtensions.kt
+++ src/main/kotlin/FileExtensions.kt
@@ -1,9 +1,13 @@
 package com.systemkern.kommons
 
 import org.w3c.dom.Document
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileInputStream
 import java.io.IOException
+import java.io.InputStreamReader
 import java.io.RandomAccessFile
+import java.nio.charset.Charset
 import javax.xml.parsers.DocumentBuilderFactory
 
 
@@ -19,8 +23,9 @@ fun File.overwrite(content: ByteArray): File {
 }
 
 fun File.ensureExists(): File {
-    this.parentFile.mkdirs()
-    if (!this.parentFile.exists())
+    parentFile.mkdirs()
+    print(parentFile)
+    if (!parentFile.exists())
         throw IOException("could not create parent folder of file with $path")
     if (!exists()) {
         createNewFile()
@@ -31,9 +36,18 @@ fun File.ensureExists(): File {
 }
 
 fun File.readAsDocument(): Document =
-        DocumentBuilderFactory.newInstance()
-                .newDocumentBuilder()
-                .parse(this)
-                .apply {
-                    documentElement.normalize()
-                }
+    DocumentBuilderFactory.newInstance()
+            .newDocumentBuilder()
+            .parse(this)
+            .apply {
+                documentElement.normalize()
+            }
+
+fun File.forEachLine(charset: Charset = Charsets.UTF_8, action: (line: String) -> Unit): Unit {
+    BufferedReader(InputStreamReader(FileInputStream(this), charset)).forEachLine(action)
+}
+
+inline fun <T> File.useLines(charset: Charset = Charsets.UTF_8, block: (Sequence<String>) -> T): T =
+        bufferedReader(charset).use {
+            block(it.lineSequence())
+        }
--- src/test/kotlin/FileExtensionsTest.kt
+++ src/test/kotlin/FileExtensionsTest.kt
@@ -1,47 +1,40 @@
 package com.systemkern.kommons
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.catchThrowable
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.io.File
-import java.io.InputStream
+import java.io.IOException
+import java.nio.charset.Charset.defaultCharset
 
 class FileExtensionsTests {
     private val pathToProject: String  = System.getProperty("user.dir")
+    private val fileName = "$pathToProject/src/test/resources/osx.txt"
+    private lateinit var file: File
 
-    @Test fun `Can read OSX Line Endings`() {
-        val fileName = "$pathToProject/src/test/resources/osx.txt"
-        print(fileName)
-        val inputStream: InputStream = File(fileName).inputStream()
-        val lineList = mutableListOf<String>()
-
-        File(fileName).useLines { lines -> lines.forEach { lineList.add(it) } }
-
-        inputStream.bufferedReader().useLines { lines ->
-            lines.forEach {
-                print(it)
-            }
-        }
+    @BeforeEach
+    fun setup(){
+        file = File(fileName)
+        file.writeText("This file\ncontains line endings\nwoho\nanother line\nEOF", defaultCharset())
     }
 
     @Test fun `Can read OSX Line Endings 2`() {
-        val fileName = ""
-        val lineList = File(fileName).readLines()
-        assertThat(lineList).isEqualTo(
-                arrayListOf<String>("This file", "contains OSX line endings", "woho", "another line", "EOF")
+        assertThat(file.readLines())
+            .isEqualTo(
+                arrayListOf("This file", "contains OSX line endings", "woho", "another line", "EOF")
+            )
+    }
+
+    @Test fun `Can override file`(){
+        val newContent = "content\nthis is the new content\nfor testing purposes"
+        assertThat(file.overwrite(newContent).readLines()).isEqualTo(
+            arrayListOf("content","this is the new content","for testing purposes")
         )
     }
-}
 
-
-/*
-
-public fun File.forEachLine(charset: Charset = Charsets.UTF_8, action: (line: String) -> Unit): Unit {
-    // Note: close is called at forEachLine
-    BufferedReader(InputStreamReader(FileInputStream(this), charset)).forEachLine(action)
-}
-inline fun <T> File.useLines(charset: Charset = Charsets.UTF_8, block: (Sequence<String>) -> T): T =
-    bufferedReader(charset).use {
-        block(it.lineSequence())
+    @Test fun `Can ensure file exists`(){
+        val fakeFileName = "some/inexistent/path/to/file.txt"
+        assertThat(catchThrowable { File(fakeFileName).ensureExists() }).isInstanceOf(IOException::class.java)
     }
- */
-
+}
